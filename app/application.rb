6503e0490af846761d7f94d64425d13cfa4d93b7
Brittany Tinnin <bntinnin7@gmail.com>
2018-08-07 12:54:40 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -34,8 +34,12 @@ class Application
 end
 
   @@cart = []
-  
+
   def handle_cart
-    
+    if @@cart.empty?
+      resp.write "Your cart is empty"
+    else
+      @@cart.each {|item| resp.write "#{item}" }
+
 
   end
