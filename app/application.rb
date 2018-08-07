2db3b0820f103d13881ac76a38c3628d111eda26
Brittany Tinnin <bntinnin7@gmail.com>
2018-08-07 12:48:40 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,22 +1,18 @@
 class Application
 
-  @@cart = ["Apples","Carrots","Pears"]
-
-  def call(env)
-    resp = Rack::Response.new
-    req = Rack::Request.new(env)
-
-    if req.path.match(/cart/)
-      if @@cart.empty?
-        resp.write "Your cart is empty"
-      else
-        @@cart.each do |item|
-          resp.write "#{item}\n"
-        end
-      end
-    elsif req.path.match(/search/)
-      search_term = req.params["q"]
-      resp.write handle_search(search_term)
+  @@items = ["Apples","Carrots","Pears"]
+ 
+   def call(env)
+     resp = Rack::Response.new
+     req = Rack::Request.new(env)
+ 
+     if req.path.match(/items/)
+       @@items.each do |item|
+         resp.write "#{item}\n"
+       end
+     elsif req.path.match(/search/)
+       search_term = req.params["q"]
+       resp.write handle_search(search_term)
     else
       resp.write "Path Not Found"
     end
